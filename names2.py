b3bbabffe785661225c6a28c02d8bc94d3bff79b
L11fe <114050712+L11fe@users.noreply.github.com>
2022-10-12 09:37:06 +0300

    Create names2.py

---
+++ names2.py
@@ -0,0 +1,22 @@
+contacts = [
+    {
+        'name': 'Anna',
+        'age': 17,
+        'phone': '26492112'
+    }, 
+    {
+        'name': 'Oskars',
+        'age': 22,
+        'phone': '26589634'
+    },
+    {
+        'name': 'Jenifer',
+        'age': 18,
+        'phone': '12789546'
+    }
+]
+
+for contact in contacts:
+    print(contact['name'], contact['age'])
+    print(contact['phone'])
+    
